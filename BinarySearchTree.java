78325d27e71b069fa30fa51603a1c65dee2a186d
Makanaka3💕 <lewis@Admins-MacBook-Air.local>
2025-03-16 11:10:49 +0200

    adjusted the code given to us

---
+++ BinarySearchTree.java
@@ -0,0 +1,104 @@
+// Hussein's Binary Search Tree
+// 27 March 2017
+// Hussein Suleman
+
+public class BinarySearchTree<dataType extends Comparable<? super dataType>> extends BinaryTree<dataType>
+{
+   public BinarySearchTree(){
+   super();
+   }
+   
+   
+    public void insert ( dataType d )
+   {
+      if (root == null)
+         root = new BinaryTreeNode<dataType> (d, null, null);
+      else
+         insert (d, root);
+   }
+
+   /**
+    * Recursively inserting a new element into the tree, starting at the given node.
+    * 
+    * @param d The data to be inserted into the tree.
+    * @param node The current node being compared against.
+    */
+   public void insert ( dataType d, BinaryTreeNode<dataType> node )
+   {
+      if (d.compareTo (node.data) <= 0)
+      {
+         if (node.left == null)
+            node.left = new BinaryTreeNode<dataType> (d, null, null);
+         else
+            insert (d, node.left);
+      }
+      else
+      {
+         if (node.right == null)
+            node.right = new BinaryTreeNode<dataType> (d, null, null);
+         else
+            insert (d, node.right);
+      }
+   }
+
+   /**
+    * Searches for an element in the Binary Search Tree.
+    * 
+    * @param d The data to search for.
+    * @return The node containing the data, or {@code null} if not found.
+    */
+   public BinaryTreeNode<dataType> find ( dataType d )
+   {
+      if (root == null)
+         return null;
+      else
+         return find (d, root);
+   }
+
+   /**
+    * Recursively searches for an element, starting at the given node.
+    * 
+    * @param d The data to search for.
+    * @param node The current node being compared against.
+    * @return The node containing the data, or {@code null} if not found.
+    */
+   public BinaryTreeNode<dataType> find ( dataType d, BinaryTreeNode<dataType> node )
+   {
+      if (d.compareTo (node.data) == 0) 
+         return node;
+      else if (d.compareTo (node.data) < 0)
+         return (node.left == null) ? null : find (d, node.left);
+      else
+         return (node.right == null) ? null : find (d, node.right);
+   }
+
+    public BinaryTreeNode<dataType> findMin ( BinaryTreeNode<dataType> node )
+   {
+      if (node != null)
+         while (node.left != null)
+            node = node.left;
+      return node;
+   }
+
+   /**
+    * Removes the minimum element from the subtree rooted at the given node.
+    * 
+    * @param node The node from which to start removing the minimum element.
+    * @return The new root of the subtree after the minimum element has been removed.
+    */
+   public BinaryTreeNode<dataType> removeMin ( BinaryTreeNode<dataType> node )
+   {
+      if (node == null)
+         return null;
+      else if (node.left != null)
+      {
+         node.left = removeMin (node.left);
+         return node;
+      }
+      else
+         return node.right;
+   }
+}
+
+//code from the given amathuba codes
+
